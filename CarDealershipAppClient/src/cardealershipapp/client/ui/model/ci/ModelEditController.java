382e672e124d99bccb80a4364bf70506d67acd56
Miroslav <kolosnjaji@stcable.net>
2024-03-01 00:45:08 +0100

    refactoring entire ModelEditForm controller class to get rid of static methods

--- CarDealershipAppClient/src/cardealershipapp/client/ui/model/ci/ModelEditController.java
+++ CarDealershipAppClient/src/cardealershipapp/client/ui/model/ci/ModelEditController.java
@@ -1,6 +1,7 @@
 package cardealershipapp.client.ui.model.ci;
 
 import cardealershipapp.client.communication.Communication;
+import cardealershipapp.client.ui.model.ModelEditForm;
 import cardealershipapp.common.domain.Brand;
 import cardealershipapp.common.domain.Model;
 import cardealershipapp.client.session.ApplicationSession;
@@ -8,6 +9,7 @@ import cardealershipapp.common.validation.InputValidationException;
 import cardealershipapp.common.transfer.Operation;
 import cardealershipapp.common.transfer.Request;
 import cardealershipapp.common.transfer.Response;
+
 import java.util.List;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
@@ -15,68 +17,73 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
- *
  * @author Miroslav Kološnjaji
  */
 public class ModelEditController {
 
-    public static void update(JTextField txtId, JTextField txtModelName, JComboBox comboBrands, JDialog dialog) {
+    private final ModelEditForm modelEditForm;
+
+    public ModelEditController(ModelEditForm modelEditForm) {
+        this.modelEditForm = modelEditForm;
+    }
+
+    public void update() {
         try {
 
-            validateInput(txtModelName);
+            validateInput();
 
-            Long modelId = Long.valueOf(txtId.getText().trim());
-            Brand brand = (Brand) comboBrands.getSelectedItem();
-            String name = txtModelName.getText().trim();
+            Long modelId = Long.valueOf(modelEditForm.getTxtId().getText().trim());
+            Brand brand = (Brand) modelEditForm.getComboBrands().getSelectedItem();
+            String name = modelEditForm.getTxtModelName().getText().trim();
 
             Model model = new Model(modelId, name, brand);
             getResponse(Operation.MODEL_UPDATE, model);
 
-            JOptionPane.showMessageDialog(dialog, "Podaci su uspesno promenjeni!");
+            JOptionPane.showMessageDialog(modelEditForm, "Podaci su uspesno promenjeni!");
 
         } catch (InputValidationException ive) {
-            JOptionPane.showMessageDialog(dialog, ive.getMessage(), "Paznja!", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(modelEditForm, ive.getMessage(), "Paznja!", JOptionPane.WARNING_MESSAGE);
         } catch (Exception ex) {
             ex.printStackTrace();
-            JOptionPane.showMessageDialog(dialog, "Desila se greska, promena podataka modela nije uspela!");
+            JOptionPane.showMessageDialog(modelEditForm, "Desila se greska, promena podataka modela nije uspela!");
         }
     }
 
-    private static void validateInput(JTextField txtModelName) throws Exception {
-        if (txtModelName.getText().trim().isEmpty() || txtModelName.getText().trim().isBlank()) {
+    private void validateInput() throws Exception {
+        if (modelEditForm.getTxtModelName().getText().trim().isEmpty() || modelEditForm.getTxtModelName().getText().trim().isBlank()) {
             throw new InputValidationException("Niste uneli naziv modela!");
         }
     }
 
-    public static void populateCombo(JComboBox comboBrands, JDialog dialog) {
+    public void populateCombo() {
         try {
 
-            List<Brand> brands = (List<Brand>)  getResponse(Operation.BRAND_GET_ALL, null).getResult();
-            brands.forEach(comboBrands::addItem);
+            List<Brand> brands = (List<Brand>) getResponse(Operation.BRAND_GET_ALL, null).getResult();
+            brands.forEach(modelEditForm.getComboBrands()::addItem);
 
         } catch (Exception ex) {
-            JOptionPane.showMessageDialog(dialog, "Doslo je do greske prilikom popunjavanja comboBox-a!", "Paznja!", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(modelEditForm, "Doslo je do greske prilikom popunjavanja comboBox-a!", "Paznja!", JOptionPane.WARNING_MESSAGE);
         }
     }
 
-    public static void prepareForm(JTextField txtId, JTextField txtModelName, JComboBox comboBrands, JDialog dialog) {
+    public void prepareForm() {
 
         try {
             Model model = ApplicationSession.getInstance().getModel();
-            txtId.setText(model.getId().toString());
-            txtModelName.setText(model.getName());
+            modelEditForm.getTxtId().setText(model.getId().toString());
+            modelEditForm.getTxtModelName().setText(model.getName());
 
             Brand brand = (Brand) getResponse(Operation.BRAND_FIND_BY_ID, model.getBrand()).getResult();
-            comboBrands.setSelectedItem(brand);
+            modelEditForm.getComboBrands().setSelectedItem(brand);
 
         } catch (Exception ex) {
             ex.printStackTrace();
-            JOptionPane.showMessageDialog(dialog, ex.getMessage(), "Paznja!", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(modelEditForm, ex.getMessage(), "Paznja!", JOptionPane.WARNING_MESSAGE);
         }
 
     }
 
-    private static Response getResponse(Operation operation, Object argument) throws Exception {
+    private Response getResponse(Operation operation, Object argument) throws Exception {
         Request request = new Request(operation, argument);
         Communication.getInstance().getSender().writeObject(request);
         Response response = (Response) Communication.getInstance().getReceiver().readObject();
